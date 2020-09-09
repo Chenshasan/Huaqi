7a14c4132e45f9e3efc740fb3609ee12772feb04
Chenshasan <48911319+Chenshasan@users.noreply.github.com>
2020-09-09 16:39:32 +0800

    Initial commit

---
+++ src/main/java/com/example/Huaqi/HuaqiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Huaqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuaqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Huaqi/HuaqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Huaqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuaqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
