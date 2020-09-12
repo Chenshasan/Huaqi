ffb4b8360683f40678b20f05ae7e4742faf6e469
Chenshasan <48911319+Chenshasan@users.noreply.github.com>
2020-09-12 14:34:37 +0800

    feat:vo po创建

--- src/main/java/com/example/Huaqi/bl/OptionService.java
+++ src/main/java/com/example/Huaqi/bl/OptionService.java
@@ -1,4 +1,5 @@
 package com.example.Huaqi.bl;
 
 public interface OptionService {
+    
 }
--- src/main/java/com/example/Huaqi/po/PutOption.java
+++ src/main/java/com/example/Huaqi/po/PutOption.java
@@ -1,4 +1,60 @@
 package com.example.Huaqi.po;
+import java.util.Date;
 
 public class PutOption {
+    String name;
+    int price;
+    int execPrice;
+    String ETFName;
+    int ETFPrice;
+    Date time;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getExecPrice() {
+        return execPrice;
+    }
+
+    public void setExecPrice(int execPrice) {
+        this.execPrice = execPrice;
+    }
+
+    public String getETFName() {
+        return ETFName;
+    }
+
+    public void setETFName(String ETFName) {
+        this.ETFName = ETFName;
+    }
+
+    public int getETFPrice() {
+        return ETFPrice;
+    }
+
+    public void setETFPrice(int ETFPrice) {
+        this.ETFPrice = ETFPrice;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+
 }
